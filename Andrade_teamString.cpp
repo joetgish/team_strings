6797977b5a677d58aebc0fd0004b95af78d2bddd
Israel Andrade <israelandrade18957@student.hartnell.edu>
2015-10-08 20:47:49 -0700

    Implemented the function isPalindrome to check if a word was a palindrome

--- Andrade_teamString.cpp
+++ Andrade_teamString.cpp
@@ -25,6 +25,8 @@ using namespace std;
 
 string initials(string strA);
 int countAlpha(string strA);
+int countWords(string strA);
+bool isPalindrome(string strA);
 
 int main()
 {
@@ -35,6 +37,16 @@ int main()
     assert(countAlpha("Maria Sera Turing") == 15);
     assert(countAlpha("Hello World") == 10);
     assert(countAlpha("Lenovo!") == 6);
+    
+    assert(countWords("Touchdown") == 1);
+    assert(countWords("STEELERS Will go to the playoffs") == 6);
+    assert(countWords("Word") == 1);
+    
+    assert((isPalindrome("racecar") == true));
+    assert((isPalindrome("redrootputuptoorder")) == true);
+    assert((isPalindrome("rotator")) == true);
+    
+    
     
     return 0;   
 }
@@ -72,4 +84,65 @@ int countAlpha(string strA)
         }
     }
     return counter;
+}
+
+int countWords(string strA)
+{
+
+    if(strA.length() == 1 && strA[0] != ' ')
+    {
+        return 1;
+    }
+    int index = strA.find(" ");
+    string newWord = strA.substr(0, index);
+    int countChars = 0;
+    int countWords = 0;
+    
+    while(strA.length() > 0)
+    {
+        strA = strA.erase(0, index);
+        
+        for(int i = 0; i < newWord.length(); i++)
+        {
+           if((strA[i] >= 65 && strA[i] <= 90) || (strA[i] >= 97 && strA[i] <= 122))
+            {
+                countChars++;
+            }
+        }
+        
+        if(strA[0] == ' ')
+        {
+            strA = strA.erase(0, 1);
+        }
+        
+        index = strA.find(" ");
+        newWord = strA.substr(0, index);
+        
+        if(countChars > 0)
+        {
+            countWords++;
+        }
+    }
+    
+    return countWords;
+}
+
+bool isPalindrome(string strA)
+{
+    string newVariable;
+    int counter = 0;
+    
+    for(int i = strA.length() -1; i >= 0; i--)
+    {
+        newVariable += strA[i];
+    }
+
+    if(strA == newVariable)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
